36706bd5d917a5b78d60ec4bb24dc0acb8e8408b
yutarou12 <yutarou12@syutarou.xyz>
2022-05-21 15:18:52 +0900

    [Fix] ephemeral=False をTrueに

--- cogs/Core.py
+++ cogs/Core.py
@@ -21,7 +21,7 @@ class Core(commands.Cog):
         現在聴いている曲を表示します。
         """
 
-        await ctx.defer(ephemeral=True)
+        await ctx.defer(ephemeral=False)
         member = ctx.guild.get_member(ctx.author.id)
         if member and member.activities:
             m_activity = None
