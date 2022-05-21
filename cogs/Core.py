b5a392f153a2136f070875e8d64578a1f04593c8
yutarou12 <yutarou12@syutarou.xyz>
2022-05-21 15:18:01 +0900

    [Fix] ephemeral=True をFalseに

--- cogs/Core.py
+++ cogs/Core.py
@@ -37,7 +37,7 @@ class Core(commands.Cog):
                 embed.add_field(name='アルバム', value=m_activity.album, inline=False)
                 embed.add_field(name='URL', value=f'[自分も聴く]({m_activity.track_url})', inline=False)
                 embed.set_image(url=m_activity.album_cover_url)
-                await ctx.respond(embed=embed)
+                await ctx.respond(embed=embed, ephemeral=False)
             else:
                 await ctx.respond('現在、Spotifyで再生していないようです。', ephemeral=True)
 
